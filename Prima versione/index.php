0b3fe5ea80efff5144af4fcdb4745d96b6cd0d8c
MicheleRai <rainonemichele1999@gmail.com>
2022-12-16 17:15:26 +0100

    first commit

---
+++ Prima versione/index.php
@@ -0,0 +1,18 @@
+<!-- 
+Stampiamo i dischi solo con l’utilizzo di PHP, che stampa direttamente i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi.
+- prima milestone: recuperare la lista dei dischi tramite una chiamata axios e stampare la lista dei titoli degli album all'interno di una lista non numerata (ul)
+- seconda milestone: per ogni disco, stampare una card con tutte le informazioni
+ -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DISCHI</title>
+</head>
+<body>
+    
+</body>
+</html>
